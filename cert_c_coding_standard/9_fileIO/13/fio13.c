2a1835fb7ee6e0f58e0f17f13122615fc2b7179f
Arvind <arvind@si-arvind.(none)>
2014-12-02 16:18:42 -0500

    Edited FIO 13 to address all conditions

--- cert_c_coding_standard/9_fileIO/13/fio13.c
+++ cert_c_coding_standard/9_fileIO/13/fio13.c
@@ -1,15 +1,10 @@
-//http://www.tutorialspoint.com/c_standard_library/c_function_ungetc.htm
-//https://www.securecoding.cert.org/confluence/display/seccode/FIO13-C.+Never+push+back+anything+other+than+one+read+character
-
-/*
-    It's unclear why this has a security impact although functionally it would be flawed code.
-*/
-
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
    FILE *fp;
+   fpos_t pos;
    int c;
    char buffer [256];
 
@@ -22,21 +17,59 @@ int main ()
    while(!feof(fp)) 
    {
       c = getc (fp);
-      /* replace ! with + */
+      printf("\nNext character is %c\n", c);
+      /* 
+        replace ! with + 
+      */
       if( c == '!' ) 
       {
+         /*
+             This edits the stream, making it available for future read and write operations 
+         */
          ungetc ('t', fp);
-         ungetc ('+', fp);
 
-         //This causes the file pointer to be reset and the ungetc() operations ignored.
-         fseek(fp, 2, SEEK_CUR);
+         /*
+            Get the current position and store it in pos.
+         */
+         if (fgetpos(fp, &pos) == -1) {
+            printf("The current file position could not be got\n");
+            exit(0);
+         }
+
+         /*
+            This moves the stream forward by 4 characters and the fputs prints the buffer out.
+         */
+         fgets(buffer, 5, fp);
+         fputs(buffer, stdout);
+         fputs("\n", stdout);
+
+         /*
+            Remember that saved position? That's restored now, so we're back at the start of the stream.
+            If we don't do this fgetpos/fsetpos thing... the ungetc won't work as expected.
+            Comment out both the fgetpos and the fsetpos and see what happens.
+         */
+         if (fsetpos(fp, &pos) == -1) {
+            printf("The current file position could not be set\n");
+            exit(0);
+         }
+
+         /*
+            This moves the stream forward by 8 characters and the fputs prints the buffer out.
+
+            At this point, in this example, all that's left after this has been read is the last ! mark. So the loop repeats, with just that
+            character pending in the stream. It's not a char by char iteration as one might intuitively expect.
+         */
+         ungetc ('+', fp);
+         fgets(buffer, 9, fp);
+         fputs(buffer, stdout);
+         fputs("\n", stdout);
+
+         fputs("Iteration done\n", stdout);
       }
       else 
       {
          ungetc(c, fp);
       }
-      fgets(buffer, 255, fp);
-      fputs(buffer, stdout);
    }
    return(0);
 }
